4c369155dfa032ef407ff760f54bd7df0404a440
mylg <mengyilangan@gmail.com>
2023-06-12 21:58:02 +0800

    fix:1071

--- everyday/src/main/java/every148/TreeAncestor.java
+++ everyday/src/main/java/every148/TreeAncestor.java
@@ -1,9 +1,6 @@
 package every148;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * https://leetcode.cn/problems/kth-ancestor-of-a-tree-node/
@@ -26,19 +23,44 @@ public class TreeAncestor {
     public TreeAncestor(int n, int[] parent) {
         this.n = n;
         this.parent = parent;
-        for (int i = 0; i < parent.length; i++) {
-            //0层
-            int parentNode = parent[i];
-            int level = parentNode + 1;
-            if (parentNode >= 0) {
-                level = nodeLevel.get(parentNode) + 1;
+        this.init();
+    }
+
+    private void init() {
+        if (parent.length <= 0) {
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < parent.length && nodeLevel.size() < n; i++) {
+            int node = i;
+            boolean add = false;
+            do {
+                int parentNode = parent[node];
+                if (parentNode == -1) {
+                    nodeLevel.put(node, 0);
+                    Set<Integer> nodeIds = nodeLevelMap.getOrDefault(0, new HashSet<>());
+                    nodeIds.add(node);
+                    nodeLevelMap.put(0, nodeIds);
+                    add = true;
+                    break;
+                }
+                stack.push(node);
+                node = parentNode;
+            } while (!nodeLevel.containsKey(node));
+
+            while (!stack.isEmpty()) {
+                node = stack.pop();
+                int parentNode = parent[node];
+                int level = nodeLevel.get(parentNode) + 1;
+                nodeLevel.put(node, level);
+                Set<Integer> nodeIds = nodeLevelMap.getOrDefault(level, new HashSet<>());
+                nodeIds.add(node);
+                nodeLevelMap.put(level, nodeIds);
             }
-            nodeLevel.put(i, level);
-            Set<Integer> nodeIds = nodeLevelMap.getOrDefault(level, new HashSet<>());
-            nodeIds.add(i);
-            nodeLevelMap.put(level, nodeIds);
         }
     }
+
 
     public int getKthAncestor(int node, int k) {
         Integer level = nodeLevel.get(node);
