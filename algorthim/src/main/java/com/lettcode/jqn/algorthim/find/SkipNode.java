6db4bfdfa44b445c7ce02646cee0eaaf0ad6e28d
mengyilangan <mengyilangan@gmail.com>
2024-03-06 11:54:35 +0800

    fix:skipTests

---
+++ algorthim/src/main/java/com/lettcode/jqn/algorthim/find/SkipNode.java
@@ -0,0 +1,17 @@
+package com.lettcode.jqn.algorthim.find;
+
+public class SkipNode {
+    int data;
+
+    SkipNode pre;
+
+    SkipNode next;
+
+    SkipNode upper;
+
+    SkipNode lower;
+
+    public SkipNode(int data) {
+        this.data = data;
+    }
+}
